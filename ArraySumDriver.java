089833db2a1f86996c6da583c4ce7c3f9d0d1f5a
mtowerss <69701161+mtowerss@users.noreply.github.com>
2021-06-20 18:54:53 -0400

    Create ArraySumDriver.java

---
+++ ArraySumDriver.java
@@ -0,0 +1,42 @@
+public class ArraySumDriver {
+	private final static int ARRAY_SIZE = 6;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int index = 0;
+
+		Integer[] myArray = new Integer[ARRAY_SIZE];
+		
+		myArray[index++] = 13;
+		myArray[index++] = 5;
+		myArray[index++] = 12;
+		myArray[index++] = 6;
+		
+		int sum = sumOfArray(myArray, 3);
+		System.out.println(sum);
+		
+		myArray[index++] = 7;
+		myArray[index++] = 1;
+		
+		sum = sumOfArray(myArray, 5);
+		System.out.println(sum);
+	}
+	
+	/**
+	 * Recursive method for generating sum of values in array
+	 * @param arr array of Integers
+	 * @param num index of array to sum all previous index values (including num)
+	 * @return 
+	 * @return sum of array values
+	 */
+	public static <T extends Number> int sumOfArray(T[] arr, int num) {
+		if (num == 0) {
+			return arr[num].intValue();
+		} else {
+			return arr[num].intValue() + sumOfArray(arr, num - 1);
+		}
+	}
+
+}
